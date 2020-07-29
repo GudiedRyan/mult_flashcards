b316d2c62f9c9b323c4b0e8de47ef32fc577e91a
Ryan Guide <ryan.guide@comcast.net>
2020-07-29 12:06:53 -0400

    Updates

---
+++ precache-manifest.cff333a61d05adbbbc668432ba2f926f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8982ed1d7a74cc6d891f1a4f743c4c",
+    "url": "/mult_flashcards/index.html"
+  },
+  {
+    "revision": "721199f6bbb5b5690722",
+    "url": "/mult_flashcards/static/css/main.d0f213b6.chunk.css"
+  },
+  {
+    "revision": "511983265d629f4c17ac",
+    "url": "/mult_flashcards/static/js/2.d7e3a30b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mult_flashcards/static/js/2.d7e3a30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721199f6bbb5b5690722",
+    "url": "/mult_flashcards/static/js/main.25f3c4b9.chunk.js"
+  },
+  {
+    "revision": "1db3cb94aec7e6a43af0",
+    "url": "/mult_flashcards/static/js/runtime-main.ee302034.js"
+  }
+]);
